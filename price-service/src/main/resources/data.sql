5cd3d0fe84f4d2477c0032464fe5dcb646326a7b
malamani2020 <70067345+malamani2020@users.noreply.github.com>
2020-08-23 19:10:13 +0530

    Add files via upload

---
+++ price-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE 'price'  (
+  'id' bigint(20) NOT NULL,
+  'name' varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+   'price' bigint(20) NULL DEFAULT NULL,
+    PRIMARY KEY ('id') USING BTREE
+);
+
